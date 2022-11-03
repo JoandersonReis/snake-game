a7451919ecf6f99639707ebd36b1ecec21b779a7
JoandersonReis <joandersonreis470@gmail.com>
2022-11-03 14:27:48 -0300

    Movimented issue not resolve

--- Snake.js
+++ Snake.js
@@ -1,11 +1,13 @@
 
 class Snake {
-  constructor(ctx) {
+  constructor(ctx, screenSize) {
     this.size = 20
-    this.speedX = 0
+    this.speedX = 1
     this.speedY = 0
-
+    
     this.context = ctx
+    this.screenSize = screenSize
+
     this.snake = [
       { x: 5, y: 2 },
       { x: 4, y: 2 },
@@ -15,16 +17,60 @@ class Snake {
   }
 
   move(position) {
-    this.snake[position] = { x: this.snake[position].x + this.speedX,  y: this.snake[position].y}
+    if(position == 0) {
+      this.snake[position] = { x: this.snake[position].x + this.speedX, y: this.snake[position].y + this.speedY}
+    } else {
+      this.snake[position] = { x: this.snake[position - 1].x + this.speedX , y: this.snake[position - 1].y + this.speedY}
+    }
+    
+  }
+  
+  changePosition() {
+    document.addEventListener("keydown", (event) => {
+      if(event.key == "ArrowUp") {
+        this.speedX = 0
+        this.speedY = -1
+      } else if(event.key == "ArrowDown") {
+        this.speedX = 0
+        this.speedY = 1
+      } else if(event.key == "ArrowLeft") {
+        this.speedX = -1
+        this.speedY = 0
+      } else if(event.key == "ArrowRight") {
+        this.speedX = 1
+        this.speedY = 0
+      }
+    })
   }
 
-  render() {
-    this.context.fillStyle = "white"
-    this.speedX = 1
+  crossWall() {
+    if(this.snake[0].x*this.size == this.screenSize + this.snake.length*this.size) {
+      this.snake[0].x = -1
+    }
 
+    if(this.snake[0].x < -1) {
+      this.snake[0].x = this.screenSize / this.size
+    }
+
+    if(this.snake[0].y*this.size == this.screenSize + this.snake.length*this.size) {
+      this.snake[0].y = -1
+    }
+
+    if(this.snake[0].y < -1) {
+      this.snake[0].y = this.screenSize / this.size
+    }
+  }
+  
+  render() {
+    this.changePosition()
+    this.crossWall()
+    this.context.stokeStyle = "black"
+    this.context.fillStyle = "white"
+    
     for(let i = 0; i < this.snake.length; i++) {
       this.move(i)
-      this.context.fillRect(this.snake[i].x*this.size, this.snake[i].y*this.size, this.size, this.size)
+      this.context.fillRect(this.snake[0].x*this.size, this.snake[0].y*this.size, this.size, this.size)
+      this.context.strokeRect(this.snake[0].x*this.size, this.snake[0].y*this.size, this.size, this.size);
     }
   }
 
--- index.js
+++ index.js
@@ -2,14 +2,16 @@ const canvas = document.getElementById("screen")
 
 const context = canvas.getContext("2d")
 
+const screenSize = canvas.height
 
-const snake = new Snake(context)
+
+const snake = new Snake(context, screenSize)
 
 
 
 function game() {
   context.fillStyle = "#000"
-  context.fillRect(0, 0, canvas.clientWidth, canvas.height)
+  context.fillRect(0, 0, screenSize, screenSize)
 
   snake.render()
 }
